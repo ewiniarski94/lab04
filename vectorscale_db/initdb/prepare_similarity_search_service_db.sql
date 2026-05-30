8ff1ef21460686d3e82844cc9da83353368f7b44
ewiniarski <ewiniarski@o2.pl>
2026-05-30 11:22:50 +0200

    push

---
+++ vectorscale_db/initdb/prepare_similarity_search_service_db.sql
@@ -0,0 +1,11 @@
+-- prepare_similarity_search_service_db.sql
+
+-- Create the database for the similarity search service
+CREATE DATABASE similarity_search_service_db;
+
+-- Connect to the database
+\c similarity_search_service_db;
+
+-- Enable vectorscale extension
+    CREATE EXTENSION IF NOT EXISTS vectorscale CASCADE;
+--       (hint: same `CREATE EXTENSION IF NOT EXISTS <name> CASCADE;` you ran in step 5)
